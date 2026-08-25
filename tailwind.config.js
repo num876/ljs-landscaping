ad2d4a8ae351e7bf90db32997af329eb06bedac6
num876 <maliknumayr@gmail.com>
2026-08-25 15:56:16 +0100

    Perf: Precompile Tailwind CSS and defer font loading

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: { sans: ['Inter', 'system-ui', 'sans-serif'] }
+    },
+  },
+  plugins: [],
+}
